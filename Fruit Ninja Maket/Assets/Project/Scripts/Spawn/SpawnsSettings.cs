c8829ef4c8ac780ff69835b13695f3a528a3ab96
Ivashin Daniil <gpanda37@gmail.com>
2021-06-09 13:06:02 +0300

    Relative sizes of spawn zones

--- Fruit Ninja Maket/Assets/Project/Scripts/Spawn/SpawnsSettings.cs
+++ Fruit Ninja Maket/Assets/Project/Scripts/Spawn/SpawnsSettings.cs
@@ -19,6 +19,10 @@ namespace Scripts.Spawn
         private float relativePostionY = 0f;
 
         [SerializeField]
+        [Range(0f, 1f)]
+        private float relativeSize = 0f;
+
+        [SerializeField]
         private SpawnZone spawnZone = null;
 
         public float Probability
@@ -45,7 +49,10 @@ namespace Scripts.Spawn
         {
             float x = Mathf.Lerp(topLeftCorner.x, bottomRightCorner.x, relativePostionX);
             float y = Mathf.Lerp(topLeftCorner.y, bottomRightCorner.y, relativePostionY);
+            float sizeX = Mathf.Lerp(topLeftCorner.x, bottomRightCorner.x, relativeSize);
+            float sizeY = Mathf.Lerp(topLeftCorner.y, bottomRightCorner.y, relativeSize);
             spawnZone.transform.position = new Vector3(x, y, 0);
+            spawnZone.transform.localScale = new Vector3(sizeX, sizeY, 1);
         }
     }
 }
