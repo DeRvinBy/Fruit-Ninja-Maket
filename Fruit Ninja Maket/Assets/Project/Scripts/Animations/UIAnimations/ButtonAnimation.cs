1bf2e8ed94b9320e6d232d06d0ad3647a811e25a
Ivashin Daniil <gpanda37@gmail.com>
2021-06-19 01:18:13 +0300

    ButtonAnimation

---
+++ Fruit Ninja Maket/Assets/Project/Scripts/Animations/UIAnimations/ButtonAnimation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts.Animations.UIAnimations
+{
+    public class ButtonAnimation : MonoBehaviour
+    {
+        [SerializeField] 
+        private float duration = 1f;
+
+        [SerializeField] 
+        private RectTransform rectTransform = null;
+        
+        [SerializeField]
+        private Ease scaleEase = Ease.Linear;
+        
+        [SerializeField] 
+        private float startScale = 1f;
+        
+        [SerializeField] 
+        private float targetScale = 0.8f;
+
+        [SerializeField] 
+        private Image image = null;
+        
+        [SerializeField]
+        private Color startColor = Color.black;
+        
+        [SerializeField]
+        private Color targetColor = Color.white;
+
+        private Sequence sequence;
+
+        private void Start()
+        {
+            image.color = startColor;
+            CreateAnimation();
+        }
+
+        private void CreateAnimation()
+        {
+            var localScale = rectTransform.localScale;
+            var halfDuration = duration / 2f;
+            var scaleParams = new TweenParams().SetEase(scaleEase);
+
+            sequence = DOTween.Sequence();
+            var scale = localScale * targetScale;
+            sequence.Append(rectTransform.DOScale(scale, halfDuration).SetAs(scaleParams));
+            sequence.Join(image.DOColor(targetColor, halfDuration));
+            scale = localScale * startScale;
+            sequence.Append(rectTransform.DOScale(scale, halfDuration).SetAs(scaleParams));
+            sequence.Join(image.DOColor(startColor, halfDuration));
+            
+            sequence.SetAutoKill(false);
+            sequence.OnComplete(() => sequence.Rewind());
+        }
+
+        public void PlayAnimation()
+        {
+            if (!sequence.IsPlaying())
+            {
+                sequence.Play();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            sequence.Kill(false);
+        }
+    }
+}
