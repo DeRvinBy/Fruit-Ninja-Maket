710228b2be0ff701f83080f7a3e5a395b57bc72a
Ivashin Daniil <gpanda37@gmail.com>
2021-06-18 12:04:08 +0300

    Random item by probability
    Added extension to get ramdon item from list by probs

---
+++ Fruit Ninja Maket/Assets/Project/Scripts/Extensions/RandomItemInList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+namespace Project.Scripts.Extensions
+{
+    public static class RandomItemInList
+    {
+        public static T GetRandomItemByProbability<T>(this IEnumerable<T> list, Func<T, float> item)
+        {
+            var sum = list.Sum(item);
+            
+            var randomPoint = Random.value * sum;
+
+            foreach (var arg in list)
+            {
+                var prob = item(arg);
+                if (randomPoint < prob)
+                {
+                    return arg;
+                }
+                else
+                {
+                    randomPoint -= prob;
+                }
+            }
+
+            return list.Last();
+        }
+    }
+}
