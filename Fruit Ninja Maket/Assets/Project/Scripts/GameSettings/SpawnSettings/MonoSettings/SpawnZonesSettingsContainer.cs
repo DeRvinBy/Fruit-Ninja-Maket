f33fa7bd07c9abd5c98afc6972e14aa37bec6e0f
Ivashin Daniil <gpanda37@gmail.com>
2021-06-18 12:09:53 +0300

    Chosing random zone by probability
    Tested chosing spawn zone by probability with extension to get random probability

--- Fruit Ninja Maket/Assets/Project/Scripts/GameSettings/SpawnSettings/MonoSettings/SpawnZonesSettingsContainer.cs
+++ Fruit Ninja Maket/Assets/Project/Scripts/GameSettings/SpawnSettings/MonoSettings/SpawnZonesSettingsContainer.cs
@@ -1,5 +1,5 @@
+using Project.Scripts.Extensions;
 using Project.Scripts.Spawn;
-using Scripts.GameSettings.SpawnSettings;
 using UnityEngine;
 
 namespace Project.Scripts.GameSettings.SpawnSettings.MonoSettings
@@ -11,57 +11,24 @@ namespace Project.Scripts.GameSettings.SpawnSettings.MonoSettings
         [SerializeField]
         private SpawnZonesSettings[] spawnZonesSettings = null;
 
-        private float[] probabilities;
-
         private void OnValidate()
         {
             if (spawnZonesSettings.Length == 0) return;
-            
-            float probabilitySum = 0;
-            foreach (var spawnZone in spawnZonesSettings)
-            {
-                if (spawnZone.Probability + probabilitySum <= ProbabilityOfAllZones)
-                {
-                    probabilitySum += spawnZone.Probability;
-                }
-                else
-                {
-                    spawnZone.Probability = ProbabilityOfAllZones - probabilitySum;
-                    probabilitySum += spawnZone.Probability;
-                }
 
-                spawnZone.UpdateSpawnZoneTransform();
-            }
+            InitializeZones();
         }
 
         public void InitializeZones()
         {
-            probabilities = new float[spawnZonesSettings.Length];
-
-            for (int i = 0; i < probabilities.Length; i++)
+            for (int i = 0; i < spawnZonesSettings.Length; i++)
             {
                 spawnZonesSettings[i].InitializeSpawnZone();
-                probabilities[i] = spawnZonesSettings[i].Probability;
             }
         }
 
         public SpawnZone GetRandomSpawnZoneByProbability()
         {
-            var randomPoint = Random.value * ProbabilityOfAllZones;
-
-            for (int i = 0; i < probabilities.Length; i++)
-            {
-                if (randomPoint < probabilities[i])
-                {
-                    return spawnZonesSettings[i].SpawnZone;
-                }
-                else
-                {
-                    randomPoint -= probabilities[i];
-                }
-            }
-
-            return spawnZonesSettings[probabilities.Length - 1].SpawnZone;
+            return spawnZonesSettings.GetRandomItemByProbability(x => x.Probability).SpawnZone;
         }
     }
 }
